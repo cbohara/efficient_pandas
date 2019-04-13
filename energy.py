4e1ac2858d33122c1187e3717a3e32349f15ede6
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-04-13 09:01:37 -0700

    leverage tutorial timer decorator, use pandas apply method

--- energy.py
+++ energy.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import pandas as pd
+from timer import timeit
 
 
 def import_file(file_name):
@@ -8,6 +10,7 @@ def import_file(file_name):
     :return: pandas dataframe
     """
     return pd.read_csv(file_name, parse_dates=[0], infer_datetime_format=True)
+
 
 def apply_tariff(kwh, hour):
     """
@@ -31,6 +34,8 @@ def apply_tariff(kwh, hour):
         raise ValueError(f'Invalid hour: {hour}')
     return rate * kwh
 
+
+@timeit(repeat=3, number=10)
 def apply_tariff_loop(df):
     """
     Use loop to add third column that provides energy cost per hour
@@ -47,7 +52,42 @@ def apply_tariff_loop(df):
     return df
 
 
+@timeit(repeat=3, number=10)
+def apply_tariff_iterrows(df):
+    """
+    Use pandas iterrows generator method to get energy cost per hour
+    :param df: dateframe to update
+    :return: updated dataframe
+    """
+    energy_cost_list = []
+    for index, row in df.iterrows():
+        energy_used = row['energy_kwh']
+        hour = row['date_time'].hour
+        energy_cost = apply_tariff(energy_used, hour)
+        energy_cost_list.append(energy_cost)
+    df['cost_cents'] = energy_cost_list
+    return df
+
+
+@timeit(repeat=3, number=10)
+def apply_tariff_appy(df):
+    """
+    Use apply() method to add third column with energy cost per hour
+    :param df: dateframe to update
+    :return: updated dataframe
+    """
+    df['cost_cents'] = df.apply(
+        lambda row: apply_tariff(
+            kwh=row['energy_kwh'],
+            hour=row['date_time'].hour
+        ), axis=1
+    )
+    return df
+
+
 if __name__ == '__main__':
     file1 = 'demand_profile.csv'
     df1 = import_file(file1)
-    updated_df1 = apply_tariff_loop(df1)
+    apply_tariff_loop(df1)
+    apply_tariff_iterrows(df1)
+    apply_tariff_appy(df1)
